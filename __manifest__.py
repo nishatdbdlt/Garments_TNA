c9ee0d9bd1f1e1a20723a077fb2b985f835f1893
md nishat <nishat7453@gmail.com>
2026-09-10 22:14:53 +0600

    Release garments TNA planning features

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Garments TNA & Templates',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.1.0',
     'category': 'Tools',
     'summary': 'Garments time and action plans with reusable, editable milestone templates',
     'depends': ['base', 'mail'],
